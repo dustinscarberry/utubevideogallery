53d97b4e3211a84ac4bf7e4d5ed3b5fbfffec671
Dustin Scarberry <dustin@codeclouds.net>
2018-09-07 00:10:50 -0400

    Added a couple components and styles

--- src/js/component/Dashboard/Dashboard.js
+++ src/js/component/Dashboard/Dashboard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Tabs from './Tabs';
 import Pane from './Pane';
+import Card from './Card';
 
 class Dashboard extends React.Component
 {
@@ -16,12 +17,27 @@ class Dashboard extends React.Component
         <h2 id="utv-masthead">uTubeVideo</h2>
 
 
+
         <Tabs selected={0}>
-          <Pane label="General" iconClass="fa-gear">
-            <p>Hi Pane</p>
+          <Pane label="Galleries" iconClass="fa-gear">
+            <Card>
+              <p>Galleries</p>
+            </Card>
           </Pane>
-          <Pane label="General2" iconClass="fa-gear">
-            <p>Hi Pane 2</p>
+          <Pane label="Playlists" iconClass="fa-gear">
+            <Card>
+              <p>Playlists</p>
+            </Card>
+          </Pane>
+          <Pane label="Settings" iconClass="fa-gear">
+            <Card>
+              <p>Settings</p>
+            </Card>
+          </Pane>
+          <Pane label="Status" iconClass="fa-gear">
+            <Card>
+              <p>Status</p>
+            </Card>
           </Pane>
         </Tabs>
       </div>
