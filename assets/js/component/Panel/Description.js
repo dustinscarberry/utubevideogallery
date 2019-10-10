97b0ce60987f66763791805de64b6a11af55aaee
Dustin Scarberry <dustin@codeclouds.net>
2019-10-10 14:45:27 -0400

    Bug fixes

--- assets/js/component/Panel/Description.js
+++ assets/js/component/Panel/Description.js
@@ -6,9 +6,10 @@ const Description = (props) =>
     text
   } = props;
 
-  if (!text)
+  //return null if no text or disabled
+  if (!text || !utvJSData.setting.showVideoDescription)
     return null;
-    
+
   return (
     <div className="utv-panel-description">
       {text}
